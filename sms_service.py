b2a252d97c923b14bb6c1a2d517e277a061cdf77
Vikram M <srevi@MacBookAir.lan>
2026-05-08 14:07:53 +0100

    Fix CORS: allow X-Edit-Token header so ai.humanagency.co can save content

--- sms_service.py
+++ sms_service.py
@@ -47,7 +47,7 @@ def _cors_headers(response):
     origin = request.headers.get("Origin", "")
     if origin in _CORS_ORIGINS:
         response.headers["Access-Control-Allow-Origin"]  = origin
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Edit-Token"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
         response.headers["Access-Control-Max-Age"]       = "600"
     return response
